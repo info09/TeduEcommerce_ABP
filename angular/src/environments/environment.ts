dc21caa6428d9f6609dd8bcbf4a96e689c69b1ab
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-05-27 09:00:46 +0700

    Testing integrate with openiddict

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,9 +1,9 @@
- import { Environment } from '@abp/ng.core';
+import { Environment } from '@abp/ng.core';
 
 const baseUrl = 'http://localhost:4200';
 
 const oAuthConfig = {
-  issuer: 'https://localhost:44377/',
+  issuer: 'https://localhost:5000/',
   redirectUri: baseUrl,
   clientId: 'TeduEcommerce_App',
   responseType: 'code',
@@ -20,7 +20,7 @@ export const environment = {
   oAuthConfig,
   apis: {
     default: {
-      url: 'https://localhost:44377',
+      url: 'https://localhost:5000',
       rootNamespace: 'TeduEcommerce',
     },
     AbpAccountPublic: {
