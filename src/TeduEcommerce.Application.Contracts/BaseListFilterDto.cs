d802a5ad51793caf638974c0bc5a363b6535a455
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-05-27 11:18:21 +0700

    feat: Create paging method using ABP

---
+++ src/TeduEcommerce.Application.Contracts/BaseListFilterDto.cs
@@ -0,0 +1,8 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace TeduEcommerce;
+
+public class BaseListFilterDto : PagedResultRequestDto
+{
+    public string? Keyword { get; set; }
+}
--- src/TeduEcommerce.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
+++ src/TeduEcommerce.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
@@ -6,4 +7,5 @@ namespace TeduEcommerce.ProductCategories;
 
 public interface IProductCategoriesAppService : ICrudAppService<ProductCategoryDto, Guid, PagedResultRequestDto, CreateUpdateProductCategoryDto, CreateUpdateProductCategoryDto>
 {
+    Task<PagedResultDto<ProductCategoryInListDto>> GetListFilterAsync(BaseListFilterDto input);
 }
