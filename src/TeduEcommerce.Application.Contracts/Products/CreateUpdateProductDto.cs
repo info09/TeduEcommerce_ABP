d38de4c18cd2e59d4587b8bca5e268d6f5c70e10
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-05-27 14:49:10 +0700

    Build form product

--- src/TeduEcommerce.Application.Contracts/Products/CreateUpdateProductDto.cs
+++ src/TeduEcommerce.Application.Contracts/Products/CreateUpdateProductDto.cs
@@ -11,7 +11,7 @@ public class CreateUpdateProductDto
     public ProductType ProductType { get; set; }
     public string SKU { get; set; }
     public int SortOrder { get; set; }
-    public bool Visiblity { get; set; }
+    public bool Visibility { get; set; }
     public bool IsActive { get; set; }
     public Guid CategoryId { get; set; }
     public string SeoMetaDescription { get; set; }
